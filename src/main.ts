620019742767aa6d451f92f8563a248502b781e4
ProUnebit <prounebit@yandex.ru>
2023-04-11 08:01:15 +0300

    added Project Type

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import "./style.scss";
 
+// Project Type
+enum ProjectStatus {
+    Active,
+    Finished,
+}
+
+class Project {
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public people: number,
+        public status: ProjectStatus
+    ) {}
+}
+
 // Project State Management
+type Listener = (items: Project[]) => void;
 
 class ProjectState {
-    private listeners: any[] = [];
-    private projects: any[] = [];
+    private listeners: Listener[] = [];
+    private projects: Project[] = [];
     private static instance: ProjectState;
 
     private constructor() {}
@@ -17,17 +34,18 @@ class ProjectState {
         return this.instance;
     }
 
-    addListener(listenerFn: Function) {
+    addListener(listenerFn: Listener) {
         this.listeners.push(listenerFn);
     }
 
     addProject(title: string, description: string, numOfPeople: number) {
-        const newProject = {
-            id: Math.random().toString(),
-            title: title,
-            description: description,
-            people: numOfPeople,
-        };
+        const newProject = new Project(
+            Math.random().toString(),
+            title,
+            description,
+            numOfPeople,
+            ProjectStatus.Active
+        );
         this.projects.push(newProject);
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
@@ -104,14 +122,14 @@ class ProjectList {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
-	assignedProjects: any[];
+    assignedProjects: Project[];
 
     constructor(private type: "active" | "finished") {
         this.templateElement = document.getElementById(
             "project-list"
         )! as HTMLTemplateElement;
         this.hostElement = document.getElementById("app")! as HTMLDivElement;
-		this.assignedProjects = [];
+        this.assignedProjects = [];
 
         const importedNode = document.importNode(
             this.templateElement.content,
